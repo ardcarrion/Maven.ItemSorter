3195f65eb65ccb29a50b3a8f87ac600e3df53be8
Alicia Carrion <alicia.r.carrion@gmail.com>
2019-06-06 08:43:02 -0400

    passes all tests including new written ones

--- src/main/java/rocks/zipcode/io/ItemSorter.java
+++ src/main/java/rocks/zipcode/io/ItemSorter.java
@@ -14,12 +14,10 @@ public class ItemSorter {
     }
 
     public Item[] sort(Comparator<Item> comparator) {
-        while (!isSorted(items), comparator) {
+        while (!isSorted(items, comparator)) {
             for (int i = 0; i < items.length - 1; i++) {
-                int compared = comparator.compare(items[i], items[i]);
-                if (compared < 0) swap(i, i + 1); //smaller than
-//            else if (compared > 0) ; //larger than
-//            else ; //same size
+                int compared = comparator.compare(items[i], items[i+1]);
+                if (compared > 0) swap(i, i + 1);
             }
         }
         return items;
@@ -32,7 +30,7 @@ public class ItemSorter {
     }
 
     private Boolean isSorted(Item[] items, Comparator<Item> comparator) {
-        for (Item item : items) {
+        for (int i = 0; i < items.length-1; i++) {
             if (comparator.compare(items[i], items[i+1]) > 0) return false;
         }
         return true;
